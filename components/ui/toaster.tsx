8699001b25cb67587089096afdded33f63ded243
Emmanuel Salazar <emmanuel.salazar@outlook.com>
2026-01-21 10:07:48 -0600

    Updated ZA-34-update-transaction-ui-adding-the-filters-search

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -8,14 +8,14 @@ import {
   ToastTitle,
   ToastViewport,
 } from "@/components/ui/toast"
-import { useToast } from "@/hooks/use-toast"
+import { useToast, type ToasterToast } from "@/components/ui/use-toast"
 
 export function Toaster() {
   const { toasts } = useToast()
 
   return (
     <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
+      {toasts.map(function ({ id, title, description, action, ...props }: ToasterToast) {
         return (
           <Toast key={id} {...props}>
             <div className="grid gap-1">
